2936a84abd05cc4285baf89ffb8436b5d189be47
Georgios Xonikis <giorgo.xonikis@gmail.com>
2019-07-07 13:20:56 +0200

    Initial Commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'PORT': 5432,
+        'HOST': 'postgres',
+        'PASSWORD': 'postgres',
+        'USER': 'postgres'
     }
 }
 
